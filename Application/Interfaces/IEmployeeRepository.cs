6fa04b6dc279eaadb353f6efe39b6acb12edc3be
Cassio Costa <cassio.costa@globalsys.com.br>
2025-12-18 18:17:12 -0300

    Add Employee Repository Interface - Contract

---
+++ Application/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,19 @@
+﻿using Application.Responses;
+using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        Task<PagedResponse<Employee>> GetPagedAsync(
+            int page,
+            int pageSize,
+            CancellationToken cancellationToken
+        );
+        Task<Domain.Entities.Employee> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+        Task CreateAsync(Employee employee, CancellationToken cancellationToken);
+        Task UpdateAsync(Employee employee, CancellationToken cancellationToken);
+        Task DeleteAsync(Guid id, CancellationToken cancellationToken);
+        Task<bool> DocumentExistsAsync(string documentNumber, CancellationToken cancellationToken);
+    }
+}
