344729e94ed48b5865313f1f787d739655d09c83
Cassio Costa <cassio.costa@globalsys.com.br>
2025-12-19 13:52:43 -0300

    Add PagedResult and used in IEmployeeRepository

--- Domain/Interfaces/IEmployeeRepository.cs
+++ Domain/Interfaces/IEmployeeRepository.cs
@@ -1,7 +1,7 @@
-﻿using Application.Responses;
+﻿using Domain.Common;
 using Domain.Entities;
 
-namespace Application.Interfaces
+namespace Domain.Interfaces
 {
     public interface IEmployeeRepository
     {
@@ -10,7 +10,7 @@ namespace Application.Interfaces
             int pageSize,
             CancellationToken cancellationToken
         );
-        Task<Domain.Entities.Employee> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+        Task<Employee> GetByIdAsync(Guid id, CancellationToken cancellationToken);
         Task CreateAsync(Employee employee, CancellationToken cancellationToken);
         Task UpdateAsync(Employee employee, CancellationToken cancellationToken);
         Task DeleteAsync(Guid id, CancellationToken cancellationToken);
