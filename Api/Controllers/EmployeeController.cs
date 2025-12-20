400db0618281345a5e082249250c5c351126b06f
Cassio Costa <cassio.costa@globalsys.com.br>
2025-12-20 02:01:09 -0300

    Add Employee Controller Remove Sample Project

---
+++ Api/Controllers/EmployeeController.cs
@@ -0,0 +1,61 @@
+﻿using Application.Commands;
+using Application.Queries;
+using Application.Requests;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public EmployeeController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var result = await _mediator.Send(new GetEmployeeByIdQuery(id));
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPaged([FromQuery] GetEmployeePagedQuery query)
+        {
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> Create([FromBody] CreateEmployeeRequest request)
+        {
+            var command = new CreateEmployeeCommand(request);
+            await _mediator.Send(command);
+
+            return Created();
+        }
+
+        [HttpPut("Update/{id}")]
+        public async Task<IActionResult> Update(Guid id, [FromBody] UpdateEmployeeRequest request)
+        {
+            var command = new UpdateEmployeeCommand(id, request);
+            await _mediator.Send(command);
+
+            return NoContent();
+        }
+
+        [HttpDelete("Delete/{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var command = new DeleteEmployeeCommand(id);
+            await _mediator.Send(command);
+
+            return NoContent();
+        }
+    }
+}
